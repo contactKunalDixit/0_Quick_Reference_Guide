b757749e2dc8f41ab7d9b874ad04e0b78a4d87d9
Kunal Dixit <contactkunaldixit@gmail.com>
2021-06-02 02:49:44 +0800

    Updated from Linux

--- 2.1 JavaScript/3.0.1 Loops in Objects- converts non Iterable Objects into Iterable Objects/app.js
+++ 2.1 JavaScript/3.0.1 Loops in Objects- converts non Iterable Objects into Iterable Objects/app.js
@@ -3,7 +3,6 @@
 
 *   The for...in statement iterates over the ENUMBERABLE properties/keys of an object, in an arbitrary order.
 
-
 ! ES6
 
 *  The better way to loop through objects is :
@@ -11,7 +10,7 @@
     2)  then, you loop through an array
 
     Convert an Object into an Array with :
-    a)  Object.keys :   creates an iterable Object(array) 
+    a)  Object.keys :   creates an iterable Object(array)
  that captures the properties(aka keys) of an object.
 
     b)  Object.values   :   creates an iterable object(array) that captures the values of every property/key in an object
@@ -19,48 +18,48 @@
     c)  Objct.entries    :  creates an iterable object(array) of arrays. Each inner array has two items. The first item is the property; the second item is the value.
 
     todo:   Once and object has been converted into an interable Object(array), all the built-in methods of Array object can be used i.e.: you can use: ...for(let prop of arr)...forEach()...filter()...map()...reduce()..etc
-    
-
 
 */
 
 const family = {
-    "father": "Kunal",
-    "mother": "Sonal",
-    "daughter": "Amaira"
-}
+  father: 'Kunal',
+  mother: 'Sonal',
+  daughter: 'Amaira',
+};
 
-// 1 Loop ES5 : for (in)            
+// 1 Loop ES5 : for (in)
 
-let loop1 = (obj) => {
-    for (key in obj) {
-        console.log(`The ${key} in family is ${obj[key]}`)
-    }
-}
+const loop1 = (obj) => {
+  for (key in obj) {
+    console.log(`The ${key} in family is ${obj[key]}`);
+  }
+};
 
-loop1(family)
+loop1(family);
 
 /*
- *  Loop ES6: 
-    *   Object.keys() 
-    *   Object.values() 
+ *  Loop ES6:
+    *   Object.keys()
+    *   Object.values()
     *   Object.entries()
  */
 
-let keys = Object.keys(family)
-console.log(keys)
+const keys = Object.keys(family);
+console.log(keys);
 
-let values = Object.values(family)
-console.log(values)
+const values = Object.values(family);
+console.log(values);
 
-let entries = Object.entries(family)
+const entries = Object.entries(family);
 console.log(entries);
 
-let entriesFunc = (arr) => {
-    arr.forEach((item) => {
-        console.log(`The ${item[0]} in the family is ${item[1]}`)
-    })
+const entriesFunc = (arr) => {
+  arr.forEach((item) => {
+    console.log(`The ${item[0]} in the family is ${item[1]}`);
+  });
+};
+entriesFunc(entries);
 
+for (let i = 25; i >= 0; i -= 5) {
+  console.log(i);
 }
-entriesFunc(entries)
-
